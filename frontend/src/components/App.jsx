ba4a651ac7a0c72a344fe19357c2b171d4608fea
tigp <hnztkfc@gmail.com>
2022-11-04 18:57:46 +0300

    added context provider change route path

--- frontend/src/components/App.jsx
+++ frontend/src/components/App.jsx
@@ -1,25 +1,35 @@
-import React from 'react';
-import {
-  BrowserRouter,
-  Routes,
-  Route,
-  Link,
-} from 'react-router-dom';
+import React, { useState, useMemo } from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Login from './Login.jsx';
 import PageNotFound from './PageNotFound.jsx';
+import AuthContext from '../context/index.jsx';
+import useAuth from '../hooks/index.jsx';
 
-const Home = () => (
-  <Link to="/login">Login</Link>
-);
+const AuthProvider = ({ children }) => {
+  const [userData, setUserData] = useState({
+    token: localStorage.getItem('token'),
+    username: localStorage.getItem('username'),
+    authState: false,
+  });
+  const memoOnUser = useMemo(() => ({ userData, setUserData }), []);
+
+  return (
+    <AuthContext.Provider value={memoOnUser}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
 
 const App = () => (
-  <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="*" element={<PageNotFound />} />
-    </Routes>
-  </BrowserRouter>
+  <AuthProvider>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={null} />
+        <Route path="/login" element={<Login />} />
+        <Route path="*" element={<PageNotFound />} />
+      </Routes>
+    </BrowserRouter>
+  </AuthProvider>
 );
 
 export default App;
