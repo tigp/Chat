54c4928b77d457ee3d587e70bd7a05bc6e28992d
tigp <hnztkfc@gmail.com>
2022-11-04 18:59:28 +0300

    changed div to the fragment <></>

--- frontend/src/components/PageNotFound.jsx
+++ frontend/src/components/PageNotFound.jsx
@@ -1,11 +1,11 @@
 import { Link } from 'react-router-dom';
 
 const PageNotFound = () => (
-  <div>
+  <>
     <h1>404 Error</h1>
     <h1>Page not found</h1>
     <Link to="/">Go to the start page!</Link>
-  </div>
+  </>
 );
 
 export default PageNotFound;
