4d612bd650bcc0ec0483d300795fb1c3e8fc24ee
tigp <hnztkfc@gmail.com>
2022-11-06 19:56:22 +0300

    created NavBar for App

---
+++ frontend/src/components/NavBar.jsx
@@ -0,0 +1,18 @@
+import { Link } from 'react-router-dom';
+import { DropdownButton, Dropdown } from 'react-bootstrap';
+
+import routes from '../routes';
+
+const NavBar = () => (
+  <nav className="shadow-sm navbar navbar-expand-lg navbar-light bg-white">
+    <div className="container">
+      <Link className="navbar-brand" to={routes.rootPagePath()}>Chat</Link>
+      <DropdownButton variant="outline-info" title="Язык" id="bg-nested-dropdown">
+        <Dropdown.Item eventKey="1">ru</Dropdown.Item>
+        <Dropdown.Item eventKey="2">en</Dropdown.Item>
+      </DropdownButton>
+    </div>
+  </nav>
+);
+
+export default NavBar;
