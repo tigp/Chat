1bf42928079ed691b80b1a44b65afab78dd75085
tigp <hnztkfc@gmail.com>
2022-11-06 19:58:31 +0300

    made pattern for paths

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -2,4 +2,8 @@ const apiPath = '/api/v1';
 
 export default {
   loginPath: () => [apiPath, 'login'].join('/'),
+  getDataPath: () => [apiPath, 'data'].join('/'),
+  rootPagePath: () => '/',
+  loginPagePath: () => '/login',
+  singUpPagePath: () => '/signup',
 };
