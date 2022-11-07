55b7fae770634194a50910f1dbe3f750a3837cde
tigp <hnztkfc@gmail.com>
2022-11-07 21:40:09 +0300

    rewrite component

--- frontend/src/components/PageNotFound.jsx
+++ frontend/src/components/PageNotFound.jsx
@@ -1,11 +1,15 @@
+import React from 'react';
 import { Link } from 'react-router-dom';
 
+import routes from '../routes';
+
 const PageNotFound = () => (
-  <>
-    <h1>404 Error</h1>
-    <h1>Page not found</h1>
-    <Link to="/">Go to the start page!</Link>
-  </>
+  <div className="text-center">
+    <h1 className="h4 text-muted">
+      Страница не найдена
+    </h1>
+    <Link to={routes.rootPagePath()}>Но вы можете перейти на главную страницу</Link>
+  </div>
 );
 
 export default PageNotFound;
