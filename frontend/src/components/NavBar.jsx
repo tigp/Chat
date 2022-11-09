484b8b28d2d01ed7c499addf5a47d74d031b334c
tigp <hnztkfc@gmail.com>
2022-11-09 15:17:27 +0300

    added lang button

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -1,5 +1,10 @@
 import { Link } from 'react-router-dom';
-import { DropdownButton, Dropdown } from 'react-bootstrap';
+import {
+  DropdownButton,
+  Dropdown,
+  Button,
+  ButtonGroup,
+} from 'react-bootstrap';
 
 import routes from '../routes';
 
@@ -7,10 +12,13 @@ const NavBar = () => (
   <nav className="shadow-sm navbar navbar-expand-lg navbar-light bg-white">
     <div className="container">
       <Link className="navbar-brand" to={routes.rootPagePath()}>Chat</Link>
-      <DropdownButton variant="outline-info" title="Язык" id="bg-nested-dropdown">
-        <Dropdown.Item eventKey="1">ру</Dropdown.Item>
-        <Dropdown.Item eventKey="2">en</Dropdown.Item>
-      </DropdownButton>
+      <ButtonGroup>
+        <DropdownButton as={ButtonGroup} title="Язык" id="bg-nested-dropdown" variant="info">
+          <Dropdown.Item eventKey="1">ру</Dropdown.Item>
+          <Dropdown.Item eventKey="2">en</Dropdown.Item>
+        </DropdownButton>
+        <Button variant="success">Выйти</Button>
+      </ButtonGroup>
     </div>
   </nav>
 );
