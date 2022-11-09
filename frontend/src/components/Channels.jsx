f697e8a3f82561a1cf93e387ff2f25a325508071
tigp <hnztkfc@gmail.com>
2022-11-09 19:25:15 +0300

    created box for channels

--- frontend/src/components/Channels.jsx
+++ frontend/src/components/Channels.jsx
@@ -1,33 +1,29 @@
 import React from 'react';
-import { Button } from 'react-bootstrap';
+import { useSelector } from 'react-redux';
 
-const Channels = ({ hi }) => {
-  console.log(hi);
+const Channels = () => {
+  const { channels } = useSelector((state) => state.channelsStore);
+
   return (
     <div className="col-4 col-md-2 border-end pt-5 px-0 bg-light">
       <div className="d-flex justify-content-between mb-2 ps-4 pe-2">
         <span>Каналы</span>
         <button type="button" className="p-0 text-primary btn btn-group-vertical">
           <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" width="20" height="20" fill="currentColor">
-            <path d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h12zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z"></path>
-            <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z"></path>
+            <path d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h12zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z" />
+            <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z" />
           </svg>
-          <span className="visually-hidden">+</span>
         </button>
       </div>
       <ul className="nav flex-column nav-pills nav-fill px-2">
-        <li className="nav-item w-100">
-          <button type="button" className="w-100 rounded-0 text-start btn btn-secondary">
-            <span className="me-1">#</span>
-            general
-          </button>
-        </li>
-        <li className="nav-item w-100">
-          <button type="button" className="w-100 rounded-0 text-start btn">
-            <span className="me-1">#</span>
-            random
-          </button>
-        </li>
+        {channels.length > 0 && channels.map(({ id, name }) => (
+          <li key={id} className="nav-item w-100">
+            <button type="button" className="w-100 rounded-0 text-start btn">
+              <span className="me-1">#</span>
+              {name}
+            </button>
+          </li>
+        ))}
       </ul>
     </div>
   );
