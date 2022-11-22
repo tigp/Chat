a2e32ed865d4395abfd87891a3cd83e17d933e00
tigp <hnztkfc@gmail.com>
2022-11-22 16:52:37 +0300

    left only the launch of the appl

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,16 +1,4 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Provider } from 'react-redux';
+import runApp from './init.js';
 
-import App from './components/App.jsx';
-import store from './slices/index.js';
-
-const root = ReactDOM.createRoot(document.querySelector('#chat'));
-root.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </React.StrictMode>,
-);
+runApp();
