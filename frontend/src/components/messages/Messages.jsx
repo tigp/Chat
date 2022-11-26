a7097358adc1932d473cf7366c476335630814c7
tigp <hnztkfc@gmail.com>
2022-11-26 17:01:08 +0300

    filtered messages by channels and fixed chat height

--- frontend/src/components/messages/Messages.jsx
+++ frontend/src/components/messages/Messages.jsx
@@ -27,7 +27,6 @@ const Messages = () => {
           text: body,
           channelId: currentChannelId,
         };
-        // console.log(messages); // array with messages
         sendNewMessage(data);
         resetForm();
       } catch (err) {
@@ -39,7 +38,7 @@ const Messages = () => {
 
   return (
     <div className="col p-0 h-100">
-      <div className="d-flex flex-column h-100">
+      <div className="d-flex flex-column chat-height">
         <div className="bg-light mb-4 p-3 shadow-sm small">
           <p className="m-0"><b># general</b></p>
           <span className="text-muted">{messages.length}</span>
--- frontend/src/components/messages/MessagesBox.jsx
+++ frontend/src/components/messages/MessagesBox.jsx
@@ -3,21 +3,19 @@ import { useSelector } from 'react-redux';
 
 const MessagesBox = () => {
   const { messages } = useSelector((state) => state.messagesStore);
-  // const { currentChannelId } = useSelector((state) => state.channelsStore);
-  // [{ name, text, channelId, id },]
+  const { currentChannelId } = useSelector((state) => state.channelsStore);
+  // messages = [{ name, text, channelId, id }, { ... }]
 
   return (
     <div id="messages-box" className="chat-messages overflow-auto px-5 ">
-      {messages.length > 0 && messages.map(({
-        name,
-        text,
-        id,
-      }) => (
-        <div key={id} className="text-break mb-2">
-          <b>{`${name}: `}</b>
-          {text}
-        </div>
-      ))}
+      {messages.length > 0 && messages
+        .filter(({ channelId }) => channelId === currentChannelId)
+        .map(({ name, text, id }) => (
+          <div key={id} className="text-break mb-2">
+            <b>{`${name}: `}</b>
+            {text}
+          </div>
+        ))}
     </div>
   );
 };
