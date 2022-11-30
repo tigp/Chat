5361f872ecff5d8e7b80dedd3a7d1bfd02d8abaa
tigp <hnztkfc@gmail.com>
2022-11-30 19:08:29 +0300

    added registration path

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -3,6 +3,7 @@ const apiPath = '/api/v1';
 export default {
   loginPath: () => [apiPath, 'login'].join('/'),
   getDataPath: () => [apiPath, 'data'].join('/'),
+  createNewUserPath: () => [apiPath, 'signup'].join('/'),
   rootPagePath: () => '/',
   loginPagePath: () => '/login',
   singUpPagePath: () => '/signup',
