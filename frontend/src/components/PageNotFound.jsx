ac267d61fc2e99f01630ddc617349a2f10b1de8a
tigp <hnztkfc@gmail.com>
2022-12-01 18:27:02 +0300

    fixed 404-page styles

--- frontend/src/components/PageNotFound.jsx
+++ frontend/src/components/PageNotFound.jsx
@@ -1,15 +1,19 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { useTranslation } from 'react-i18next';
 
 import routes from '../routes';
 
-const PageNotFound = () => (
-  <div className="text-center">
-    <h1 className="h4 text-muted">
-      Страница не найдена
-    </h1>
-    <Link to={routes.rootPagePath()}>Но вы можете перейти на главную страницу</Link>
-  </div>
-);
+const PageNotFound = () => {
+  const { t } = useTranslation();
+  return (
+    <div className="text-center page-404-margin-top">
+      <h1 className="h4 text-muted">
+        {t('pageNotFound.header')}
+      </h1>
+      <Link to={routes.rootPagePath()}>{t('pageNotFound.link')}</Link>
+    </div>
+  );
+};
 
 export default PageNotFound;
