c5062793f29e97407a220ed8cdbf3d2ea7e0b65a
tigp <hnztkfc@gmail.com>
2022-12-01 16:59:55 +0300

    added language switcher

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -13,16 +13,17 @@ import routes from '../routes';
 
 const NavBar = () => {
   const { user, logOut } = useAuth();
-  const { t } = useTranslation();
+  const { t, i18n } = useTranslation();
 
   return (
     <nav className="shadow-sm navbar navbar-expand-lg navbar-light bg-white">
       <div className="container">
         <Link className="navbar-brand" to={routes.rootPagePath()}>{t('navBar.chatLinks')}</Link>
+        {user && <b>{`Welcome ${user.username}!`}</b>}
         <ButtonGroup>
           <DropdownButton as={ButtonGroup} title={t('navBar.languageSelection')} id="bg-nested-dropdown" variant="info">
-            <Dropdown.Item eventKey="1">{t('navBar.ru')}</Dropdown.Item>
-            <Dropdown.Item eventKey="2">{t('navBar.en')}</Dropdown.Item>
+            <Dropdown.Item eventKey="1" onClick={() => i18n.changeLanguage('ru')}>{t('navBar.ru')}</Dropdown.Item>
+            <Dropdown.Item eventKey="2" onClick={() => i18n.changeLanguage('en')}>{t('navBar.en')}</Dropdown.Item>
           </DropdownButton>
           {user && <Button variant="success" onClick={() => logOut()}>{t('navBar.logoutButton')}</Button>}
         </ButtonGroup>
