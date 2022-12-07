e5ce0922aecacc8500b63b3d270e579cfa50a0c6
tigp <hnztkfc@gmail.com>
2022-12-07 13:20:27 +0300

    added text from i18next library

--- frontend/src/components/Registration.jsx
+++ frontend/src/components/Registration.jsx
@@ -127,7 +127,7 @@ const Registration = () => {
                     name="confirmPassword"
                     autoComplete="confirmPassword"
                     required=""
-                    placeholder="Пароли должны совпадать"
+                    placeholder={t('errors.confPassword')}
                     id="confirmPassword"
                     type="password"
                   />
